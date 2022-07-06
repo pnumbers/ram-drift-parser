8ff8b36733065fecb3c8a83366f3931b154d84ab
Paul Numbers <pnumbers210@gmail.com>
2022-07-06 10:34:38 -0400

    Added Torsional Irregularity Parsing Function

--- drift_parser.py
+++ drift_parser.py
@@ -1,14 +1,18 @@
 import csv
 from dataclasses import dataclass
 import re
-from stat import FILE_ATTRIBUTE_ARCHIVE
+import os
 
 FILE = "raw_data.csv"
 FILE_PATH = FILE
 
-
+# TODO Add in arg parsing for commandline runs
+# TODO Create a way to parse drfit outputs to give deflections
+# TODO Add in commandline output of deflections and torsion irregularity
+# TODO Add in support to parse the data for torsional irregularity without
+# the use of RAM's torsional data based on the drift only cases
 class RamDriftImporter:
-    """A class that imports and parses rift output from RAM Structural
+    """A class that imports and parses drift output from RAM Structural
     System's Frame module. This class requires control points to have
     been set in RAM Frame."""
 
@@ -19,9 +23,15 @@ class RamDriftImporter:
         self.results_title_index = None
         self.load_cases_title_index = None
         self.torsional_title_index = None
-        #
+        # Data containers for the parsed data
         self.load_cases = {}
         self.drift_data = {}
+        self.torsion_data = {}
+        # Automatically run the parser if an input file is given
+        if self.import_file_path and os.path.exists(self.import_file_path):
+            self.import_drift_data()
+            self.parse_data()
+            return (self.load_cases, self.drift_data, self.torsion_data)
 
     def import_drift_data(self) -> None:
         """Imports the raw data csv file from RAM and stores the data
@@ -64,8 +74,6 @@ class RamDriftImporter:
         location = None
         story = None
 
-        print(self.load_cases.keys())
-
         # Loop through the drift data block. This includes header
         # information for each control point
         for i in range(self.results_title_index + 1, self.torsional_title_index):
@@ -104,12 +112,6 @@ class RamDriftImporter:
                 data["displacement"] = displacement
                 data["story_drift"] = story_drift
                 data["drift_ratio"] = drift_ratio
-                # displacement_x = float(row[2])
-                # displacement_y = float(row[3])
-                # story_drift_x = float(row[4])
-                # story_drift_y = float(row[5])
-                # drift_ratio_x = float(row[6])
-                # drift_ratio_y = float(row[7])
 
                 control_point_data["drifts"][story][load_case] = data
 
@@ -117,23 +119,82 @@ class RamDriftImporter:
         self.drift_data[control_point_name] = control_point_data
 
     def parse_torsional_data(self):
-
+        """Parses the 'Torsional Irregularity Data from the input file"""
         axis = None
-
         for i in range(self.torsional_title_index + 1, len(self.data)):
             row = self.data[i]
             if "-Axis" in row[0]:
+                if axis:
+                    self.torsion_data[axis] = axis_dict
                 axis = row[0]
+                axis_dict = {}
+                skip = 2
+                continue
+
+            if skip:
+                skip -= 1
+                continue
+
+            story = row[0]
+            load_case = row[1]
+            # Check is the load case is not a drift case. If it is not a
+            #  drift case then the data is not valid
+            if load_case not in self.load_cases:
+                self.torsion_data = None
+                break
+
+            max_drift = float(row[2])
+            x = re.search("\(-?\d+.\d*", row[3]).group(0)[1:]
+            y = re.search("\s-?\d+.\d*", row[3]).group(0)[1:]
+            max_drift_coord = (float(x), float(y))
+
+            min_drift = float(row[4])
+            x = re.search("\(-?\d+.\d*", row[5]).group(0)[1:]
+            y = re.search("\s-?\d+.\d*", row[5]).group(0)[1:]
+            min_drift_coord = (float(x), float(y))
+
+            max_div_min = float(row[6])
+            max_div_avg = float(row[7])
+            Ax = round(min(max(((max_div_avg / 1.2) ** 2), 1), 3), 4)
+            percent_eccentricity = round(Ax * 0.05 * 100, 4)
+
+            story_dict = {}
+            story_dict["Load Case"] = load_case
+            story_dict["Max Drift"] = max_drift
+            story_dict["Max Drift Coord"] = max_drift_coord
+            story_dict["Min Drift"] = min_drift
+            story_dict["Min Drift Coord"] = min_drift_coord
+            story_dict["Max/Min"] = max_div_min
+            story_dict["Max/Avg"] = max_div_avg
+            story_dict["Ax"] = Ax
+            story_dict["Eccentricity"] = percent_eccentricity
+
+            axis_dict[story] = story_dict
+
+        # Add final axis dictionary to class torsion data
+        if self.torsion_data:
+            self.torsion_data[axis] = axis_dict
+
+    def parse_data(self):
+        self.get_section_indexes()
+        self.get_load_cases()
+        self.parse_drift_data()
+        self.parse_torsional_data()
+
+    # This function isn't currently all that useful. It prints too
+    # data and the data is too messy to read. Consider a better way to
+    # handle this. Also consider if this is necessary.
+    def print_data(self):
+        print(self.load_cases)
+        print(self.drift_data)
+        print(self.torsion_data)
 
 
 def main():
     drift_importer = RamDriftImporter(FILE_PATH)
-    drift_importer.import_drift_data()
+    # drift_importer.import_drift_data()
     # drift_importer.parse_data()
-    drift_importer.get_section_indexes()
-    drift_importer.get_load_cases()
-    drift_importer.parse_drift_data()
-    print(drift_importer.load_cases)
+    # drift_importer.print_data()
 
 
 if __name__ == "__main__":
